d8a38ef499f130bd43c3ecb1e51c18b3a057f225
Loic2dot0 <moreaux.loic@gmail.com>
2023-04-05 15:02:18 +0200

    import de l'app dans le server ajout de vérification du port

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 require('dotenv').config();
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('Node server started');
-});
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+    if(isNaN(port)) return val;
+    if (port >= 0) return port;
+    return false;
+}
 
-server.listen(process.env.PORT || 3000);
+const port = normalizePort(process.env.PORT || '3000');
+
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
