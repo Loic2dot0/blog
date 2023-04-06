3a621223bd4bb0c89b8cc7ed9c6e67e2e7b453f3
Loic2dot0 <moreaux.loic@gmail.com>
2023-04-06 10:17:34 +0200

    création du model PostComment

---
+++ backend/models/PostComment.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../services/sequelize');
+
+const PostComment = sequelize.define('Post_Comment', {   
+    id_comment: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        unique: true,
+    },  
+    id_post: {  // id du post  (clé étrangère)  
+        type: DataTypes.UUID,
+        allowNull: false,
+    },
+    id_user: {  // id de l'utilisateur  (clé étrangère) 
+        type: DataTypes.UUID,
+        allowNull: false,
+    },      
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    }  
+}, {
+    // options    
+});
+
+module.exports = PostComment;
