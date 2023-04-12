d302686cb24743f706cd0678c038fb7bb03d9386
Loic2dot0 <moreaux.loic@gmail.com>
2023-04-12 16:40:00 +0200

    création du controller deleteUser

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -94,3 +94,30 @@ exports.updateUser = async (req, res, next) => {
         })
         .catch(error => res.status(500).json({error: error}));
 };
+
+exports.deleteUser = async (req, res, next) => {
+    const user = await User.findOne({
+        where: {
+            id_user: req.headers.userid
+        }
+    });
+
+    if (!user) return res.status(404).json({error: 'User not found!'});
+
+    const avatarPath = user.avatar ? user.avatar.split('/upload/')[1] : null;
+    
+    User.destroy({
+        where: {
+                id_user: req.headers.userid
+            }
+        })
+        .then(() => {
+            if(avatarPath){
+                fs.unlink(`upload/${avatarPath}`, (err) => {
+                    if(err) throw err;
+                });
+            }            
+            res.status(200).json({message: 'User deleted!'});
+        })
+        .catch(error => res.status(500).json({error: error}));
+};
