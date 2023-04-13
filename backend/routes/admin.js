81a27319cb95f1235db13224eea572e0686aa4f9
Loic2dot0 <moreaux.loic@gmail.com>
2023-04-13 12:00:51 +0200

    création du controller admin.js transfert de la fonction de loginAdmin vers le nouveau fichier modification des routes admin

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const auth = require('../middleware/auth');
 const adminCtrl = require('../controllers/admin');
 
 // Routes réservée aux administrateurs
-router.post('/login', userCtrl.loginAdmin);
-router.get('/', auth, userCtrl.getAdmin);
+router.post('/login', adminCtrl.loginAdmin);
 
 module.exports = router;
