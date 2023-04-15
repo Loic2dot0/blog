01c3b78fc5cfd79beb51c269ee782f05dbd18467
Loic2dot0 <moreaux.loic@gmail.com>
2023-04-15 18:52:26 +0200

    création des controllers PostReaction

---
+++ backend/controllers/postReaction.js
@@ -0,0 +1,67 @@
+const PostReaction = require('../models/PostReaction');
+
+// Create or Update a reaction
+exports.createOrUpdateReaction = async (req, res, next) => {
+    let userReaction;
+    try {
+        userReaction = await PostReaction.findOne({
+            where: {
+                id_user: req.headers.userid,
+                id_post: req.params.id_post
+            }
+        });
+    } catch (error) {
+        return res.status(500).json({error : error});
+    }
+
+    if(userReaction) {
+        if(req.body.reaction) userReaction.reaction = req.body.reaction;
+        userReaction.save()
+            .then(() => res.status(200).json({
+                message: 'Reaction modified!',
+                reaction: userReaction.reaction}
+            ))
+            .catch(error => res.status(500).json({error: error}));
+    } else {
+        PostReaction.create({
+                id_user: req.headers.userid,
+                id_post: req.params.id_post,
+                reaction: req.body.reaction,
+            })
+            .then(() => res.status(201).json({
+                message: 'Reaction created!',
+                reaction: req.body.reaction
+            }))
+            .catch(error => res.status(500).json({error: error}));
+    }
+};
+
+// Get all reactions of a post
+exports.getAllReactions = async (req, res, next) => {
+    let userReaction;
+    console.log(userReaction)
+    if(req.headers.userid){
+        try {
+            userReaction = await PostReaction.findOne({
+                where: {
+                    id_user: req.headers.userid,
+                    id_post: req.params.id_post
+                }
+            });
+        } catch (error) {
+            return res.status(500).json({error : error});
+        }
+    }
+
+    PostReaction.findAll({
+            where: {
+                id_post: req.params.id_post
+            },
+            exclude: ['id_user', 'id_post']
+        })
+        .then(reactions => res.status(200).json({
+            reactions,
+            userReaction: userReaction === undefined ? undefined : userReaction.reaction
+        }))
+        .catch(error => res.status(500).json({error: error}));
+};
