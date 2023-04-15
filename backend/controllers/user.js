e4fb13ddcef85499c104dabdbe8c2c49015c912a
Loic2dot0 <moreaux.loic@gmail.com>
2023-04-15 17:33:32 +0200

    Modification du controller login ajout d'une clé dans le .env ajout d'exemple dans le .env

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const CryptoJS = require('crypto-js');
 const fs = require('fs');
 const User = require('../models/User');
+const Admin = require('../models/Admin');
 
 // signup a new user
 exports.signup = (req, res, next) => {
@@ -24,30 +25,46 @@ exports.signup = (req, res, next) => {
 };
 
 // login to the user account
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
     const key = CryptoJS.enc.Hex.parse(process.env.CRYPTO_KEY);
     const iv = CryptoJS.enc.Hex.parse(process.env.CRYPTO_IV);
-    
-    User.findOne({
+
+    let user;
+    try{
+        user = await User.findOne({
             where: {
                 email: CryptoJS.AES.encrypt(req.body.email, key, {iv: iv}).toString()
             }
-        })
-        .then(user => {
-            if(!user) return res.status(404).json({error: 'User not found!'});
+        });
+        
+        if (!user) return res.status(404).json({error: 'User not found!'});
+    } catch (error) {
+        return res.status(500).json({error: error});
+    }
+
+    let isAdmin;
+    try {
+        isAdmin = await Admin.findOne({
+            where: {
+                id_user: user.id_user
+            }
+        });
+    } catch (error) {
+        return res.status(500).json({error: error});
+    }
+
+    bcrypt.compare(req.body.password, user.password)
+        .then(valid =>{
+            if(!valid) return res.status(401).json({error: 'Incorrect password!'});
             
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid =>{
-                    if(!valid) return res.status(401).json({error: 'Incorrect password!'});
-                    
-                    res.status(200).json({
-                        userId: user.id_user,
-                        name: user.name,
-                        avatar: user.avatar,
-                        token: jwt.sign({ userId: user.id_user }, process.env.JWT_KEY, { expiresIn: '24h' })
-                    });
-                })
-                .catch(error => res.status(500).json({error}));
+            res.status(200).json({
+                userId: user.id_user,
+                token: jwt.sign({ userId: user.id_user }, process.env.JWT_KEY, { expiresIn: process.env.JWT_EXPIRATION }),
+                name: user.name,
+                avatar: user.avatar,
+                isAdmin: isAdmin ? true : false
+                
+            });
         })
         .catch(error => res.status(500).json({error}));
 };
