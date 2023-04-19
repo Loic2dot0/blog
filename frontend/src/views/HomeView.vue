7faebd74206ce1e541cc035a181f7e60e5eb50ff
Loic2dot0 <moreaux.loic@gmail.com>
2023-04-19 19:43:53 +0200

    modification de HomeView ajout de la récupération vi l'api et affichage des catégories ajout de la récupération via l'api des articles installation d'axios création de variable d'environnement

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,74 @@
-<script setup>
+<template>
+  <h2>Articles</h2>
 
+  <div class="category" v-if="categories.length != 0">
+    <label for="category">Catégorie : </label>
+    <select name="category" id="category" v-model="categorySelected">
+      <option value="null">Toutes</option>
+      <option v-for="(category, index) in categories" :key="index" :value="category.id_category">
+        {{ category.category }}
+      </option>
+    </select>
+  </div>
+
+  <p v-if="isLoading">chargement des articles en cours...</p>
+  <p v-else-if="posts.count == 0">Aucun article</p>
+  <p v-else>{{ posts }}</p>
+  
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: 'HomeView',
+    data() {
+      return {
+        categories: [],
+        posts: [],
+        categorySelected: null,
+        page: 1,
+        isLoading: false
+      }
+    },
+    watch: {
+      categorySelected(){
+        this.page = 1;
+        this.getPosts()
+      }
+    },
+    methods: {
+      getCategories(){
+        axios.get(`${import.meta.env.VITE_URL_API}/postcategory`)
+          .then(res => {
+            this.categories = res.data
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      },
+      getPosts(){
+        this.isLoading = true;
+        axios.get(`${import.meta.env.VITE_URL_API}/post?page=${this.page}${this.categorySelected && this.categorySelected != 'null' ? '&category=' + this.categorySelected : ''}`)
+          .then(res => {
+            this.posts = res.data
+          })
+          .catch(error => {
+            console.log(error)
+          })
+          .finally(() => {
+            this.isLoading = false;
+          })
+      },
+
+    },
+      created() {
+      this.getCategories();
+      this.getPosts();
+    }
+  }
 </script>
 
-<template>
-  Home
-</template>
+<style lang="scss" scoped>
+
+</style>
