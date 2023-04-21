c6a25a93234ab2ce457889f9c969be5be5e8ae85
Loic2dot0 <moreaux.loic@gmail.com>
2023-04-21 18:27:48 +0200

    modification de HomeView ajout des articles récents modification du style

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,28 +2,67 @@
     <h2>Les derniers articles</h2>
 
     <div class="recent-container">
-        <RouterLink to="/post" class="button">Voir tous les articles</RouterLink>
+        <PostCard v-for="(post, index) in posts" :key="index" :post="post"></PostCard>
+        <div class="recent-container__link">
+            <RouterLink to="/post" class="button">Voir tous les articles</RouterLink>
+        </div>
     </div>
 
     <h2>Les derniers albums</h2>
     
     <div class="recent-container">
-        <RouterLink to="/photos" class="button">Voir tous les articles</RouterLink>
+
+        <div class="recent-container__link">
+            <RouterLink to="/photos" class="button">Voir tous les albums</RouterLink>
+        </div>
     </div>
 
     <h2>Les dernières vidéos</h2>
 
     <div class="recent-container">
-        <RouterLink to="/videos" class="button">Voir tous les articles</RouterLink>
+        
+        <div class="recent-container__link">
+            <RouterLink to="/videos" class="button">Voir tous les vidéos</RouterLink>
+        </div>
     </div>
 
 </template>
  
 <script>
 import { RouterLink } from 'vue-router';
+import axios from 'axios';
+
+import PostCard from '../components/PostCard.vue';
 
 export default {
     name: 'HomeView',
+    components: {
+        PostCard,
+    },
+    data() {
+        return {
+            posts: [],
+            isLoading: false,
+        }
+    },
+    methods: {
+      getPosts(){
+        this.isLoading = true;
+        axios.get(`${import.meta.env.VITE_URL_API}/post?limit=3`)
+          .then(res => {
+            this.posts = res.data.rows;
+          })
+          .catch(error => {
+            console.log(error)
+          })
+          .finally(() => {
+            this.isLoading = false;
+          })
+      },
+    },
+    created() {
+      this.getPosts();
+    }
 };
 </script>
 
