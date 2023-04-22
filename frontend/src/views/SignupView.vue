99ccc83f2ede11f5924d2c69f58f37b670163580
Loic2dot0 <moreaux.loic@gmail.com>
2023-04-22 20:17:32 +0200

    création de Signup

---
+++ frontend/src/views/SignupView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        Signup
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SignupView',
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+
+        },
+        watch: {
+
+        },
+        created() {
+
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
