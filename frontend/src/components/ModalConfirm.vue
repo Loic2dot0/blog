2844c713ab0f29f48bdf11c7c9935cdba4073dc5
Loic2dot0 <moreaux.loic@gmail.com>
2023-04-26 20:40:11 +0200

    création de ModalConfirm création d'un modal pour la confirmation de suppression mise à jour du style

---
+++ frontend/src/components/ModalConfirm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="overlay" @click="cancelAction">
+        <div class="modal" @click.stop>
+            <p class="modal__title">{{ title }}</p>
+            <div class="modal__group">
+                <button class="button button--danger" @click="cancelAction">Annuler</button>
+                <button class="button button--success" @click="confirmAction">Confirmer</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ModalConfirmation',
+        emits: ['confirm', 'cancel'],
+        props: {
+            title: {
+                type: String,
+                required: true
+            }
+        },
+        methods: {
+            confirmAction(){
+                this.$emit('confirm');
+            },
+            cancelAction(){
+                this.$emit('cancel');
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
