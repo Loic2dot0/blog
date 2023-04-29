c7a53fd95b02a24bc61e4deff7a884d5ed4010a8
Loic2dot0 <moreaux.loic@gmail.com>
2023-04-29 16:56:11 +0200

    ajout de nouvelle routes user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -10,6 +10,8 @@ const userCtrl = require('../controllers/user');
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 router.get('/', auth, userCtrl.getUser);
+router.get('/email', userCtrl.getUserEmailExist);
+router.get('/name', userCtrl.getUserNameExist);
 router.put('/', auth, multer, userCtrl.updateUser);
 router.delete('/', auth, userCtrl.deleteUser);
 router.delete('/:id_user', auth, authAdmin, userCtrl.deleteAUser);
